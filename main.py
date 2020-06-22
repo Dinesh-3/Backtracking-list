32af2597877550d3f16749ca3a9675a43ac27158
Dinesh I <62064640+Dinesh-3@users.noreply.github.com>
2020-06-22 05:48:35 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def print_list_elements(array):
+    for element in array:
+        if type(element) is list:
+            print_list_elements(element)
+        else:
+            print(element)
+array = [0,
+    [1,2,3],
+    [4,5,6,7,8,[1,2,3,4]],
+    [9,10,11,[100,101,[12,45,9],95],12,13],
+    14,
+    [15,16],
+    17,
+    ]        
+sub_list(array)    
